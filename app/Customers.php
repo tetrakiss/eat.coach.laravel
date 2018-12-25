a3931a44197156872dbd1bcdf512453cddded234
tetrakiss <togulev@gmail.com>
2018-12-25 13:43:57 +0300

    add models

---
+++ app/Customers.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customers extends Model
+{
+  /**
+   * The table associated with the model.
+   *
+   * @var string
+   */
+  protected $table = 'customers';
+
+
+  /**
+   * Customer может иметь множество детей.
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasMany
+   */
+    public function children()
+    {
+        return $this->hasMany(Children::class);
+    }
+
+    /**
+     * Add a children to customer.
+     *
+     * @param  array $reply
+     * @return Model
+     */
+    public function addChildren($children)
+    {
+        $children = $this->children()->create($children);
+        return $children;
+    }
+
+    /**
+     * Customer может иметь множество комментарием.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+      public function comments()
+      {
+          return $this->hasMany(Comments::class);
+      }
+
+      /**
+       * Add a commnet to customer.
+       *
+       * @param  array $reply
+       * @return Model
+       */
+      public function addComment($comment)
+      {
+          $comment = $this->comments()->create($comment);
+          return $comment;
+      }
+
+      /**
+       * Customer может иметь множество назначений.
+       *
+       * @return \Illuminate\Database\Eloquent\Relations\HasMany
+       */
+        public function appointments()
+        {
+            return $this->hasMany(Appointments::class);
+        }
+
+        /**
+         * Add a appointment to customer.
+         *
+         * @param  array $reply
+         * @return Model
+         */
+        public function addAppointment($appointment)
+        {
+            $appointment = $this->appointments()->create($appointment);
+            return $appointment;
+        }
+
+}
--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,18 +42,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+          //'unix_socket' => env('DB_SOCKET','/tmp/mysql.sock'),
+          //  'charset' => 'utf8',
+          //  'collation' => 'utf8_unicode_ci',
+          //  'prefix' => '',
+          //  'prefix_indexes' => true,
+          //  'strict' => FALSE,
+          //  'engine' => null,
         ],
 
         'pgsql' => [
