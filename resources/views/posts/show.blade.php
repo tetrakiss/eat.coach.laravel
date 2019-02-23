505db87cc543541f5929586b088ce919e4d74d82
Arseny <togulev@gmail.com>
2019-02-23 18:03:05 +0300

    add first section of image optimization

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,7 +10,7 @@
 
     <canvas class="uk-dark" width="100%" height="400"></canvas>
     <div style="z-index:1200;" class=" uk-position-center"><h1 style="color:white!important;">{{$post->title}}</h1></div>
-    {!! Html::image($post->title_image ,'post-cover', ['uk-cover', 'class'=>'uk-dark'] ) !!}
+    {!! Html::image( asset('uploads/posts/large_photos/'.$post->title_image) ,'post-cover', ['uk-cover', 'class'=>'uk-dark'] ) !!}
 </div>
 <div uk-grid>
 
