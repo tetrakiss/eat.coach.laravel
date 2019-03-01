29e4dd250ff49e4790a0e28d85a82bafb54fb45e
Arseny <togulev@gmail.com>
2019-03-01 20:50:41 +0300

    add messenger

--- resources/views/layouts/eat_front.blade.php
+++ resources/views/layouts/eat_front.blade.php
@@ -48,6 +48,32 @@
 @yield('custom_head_scr')
 </head>
 <body>
+  <!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v3.2'
+    });
+  };
+
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/ru_RU/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="1685848014779114"
+  theme_color="#c84033"
+  logged_in_greeting="Здравствуйте! Чем Мы можем Вам помочь?"
+  logged_out_greeting="Здравствуйте! Чем Мы можем Вам помочь?">
+</div>
   <div class="uk-position-relative">
 
       <nav class="uk-navbar-container white" uk-navbar="dropbar: true; align:center;">
