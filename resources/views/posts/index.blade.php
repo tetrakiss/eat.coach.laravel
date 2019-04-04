5c82ab459adf352f0ea7f8c30170984533e25520
Arseny <togulev@gmail.com>
2019-04-04 09:51:43 +0300

    add new avatar

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
 <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
     <div class="uk-card-media-left uk-cover-container">
 
-        <img src="{{asset('/images/avatar-02-15-19.jpg')}}"  uk-cover>
+        <img src="{{asset('/images/avatar-04-04-19.jpg')}}"  uk-cover>
         <canvas width="500" height="500"></canvas>
     </div>
     <div>
