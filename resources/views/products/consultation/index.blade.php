b556e6baa685772876472cd59e457e0de457cca4
Arseny <togulev@gmail.com>
2019-08-22 23:41:48 +0300

    add new tarifs

--- resources/views/products/consultation/index.blade.php
+++ resources/views/products/consultation/index.blade.php
@@ -10,6 +10,28 @@
 
   <div>
         <div class="uk-card uk-card-hover  uk-card-default">
+          <div class="uk-card-badge uk-label">3000 ₽</div>
+          <div class="uk-card-header">
+                <h3 class="uk-card-title uk-margin-remove-bottom">Первичная консультация по акции</h3>
+            </div>
+
+            <div class="uk-card-body">
+              <p><b>Включает в себя:</b></p>
+              <ul class="uk-list uk-list-striped">
+                  <li>Консультация в Skype длительностью 50-60 минут</li>
+                  <li>Сбор анамнеза и обработка анкет</li>
+                  <li>Рекомендации по коррекции пищевого поведения (разработка стратегии коррекции)</li>
+                  <li><a href="{{ url('how') }}">Как проходит консультация?</a></li>
+              </ul>
+
+            </div>
+            <div class="uk-card-footer">
+                <a href="{{route('yandex.create', ['type' => 3])}}" class="uk-button uk-button-text">Оплатить</a>
+            </div>
+        </div>
+    </div>
+  <div>
+        <div class="uk-card uk-card-hover  uk-card-default">
           <div class="uk-card-badge uk-label">4000 ₽</div>
           <div class="uk-card-header">
                 <h3 class="uk-card-title uk-margin-remove-bottom">Первичная консультация</h3>
@@ -48,6 +70,49 @@
           </div>
           <div class="uk-card-footer">
                 <a href="{{route('yandex.create', ['type' => 2])}}" class="uk-button uk-button-text">Оплатить</a>
+          </div>
+      </div>
+    </div>
+    <div>
+      <div class="uk-card uk-card-hover uk-card-default">
+        <div class="uk-card-badge uk-label">5000 ₽</div>
+        <div class="uk-card-header">
+              <h3 class="uk-card-title uk-margin-remove-bottom">Первичная консультация "1+1"</h3>
+          </div>
+          <div class="uk-card-body">
+            <p><b>Включает в себя:</b></p>
+            <ul class="uk-list uk-list-striped">
+              <li>Консультация для Родителя и ребенка или 2 детей из одной семьи</li>
+                <li>Консультация в Skype длительностью  90 минут</li>
+                <li>Сбор анамнеза и обработка анкет</li>
+                <li>Рекомендации по коррекции пищевого поведения (разработка стратегии коррекции)</li>
+                <li><a href="{{ url('how') }}">Как проходит консультация?</a></li>
+            </ul>
+
+          </div>
+          <div class="uk-card-footer">
+                <a href="{{route('yandex.create', ['type' => 4])}}" class="uk-button uk-button-text">Оплатить</a>
+          </div>
+      </div>
+    </div>
+    <div>
+      <div class="uk-card uk-card-hover uk-card-default">
+        <div class="uk-card-badge uk-label">4000 ₽</div>
+        <div class="uk-card-header">
+              <h3 class="uk-card-title uk-margin-remove-bottom">Повторная консультация "1+1"</h3>
+          </div>
+          <div class="uk-card-body">
+            <p><b>Включает в себя:</b></p>
+            <ul class="uk-list uk-list-striped">
+              <li>Консультация для Родителя и ребенка или 2 детей из одной семьи</li>
+                <li>Консультация в Skype длительностью  90 минут</li>
+                <li>Уточнение (изменение) стратегии коррекции пищевого поведения</li>                
+                <li><a href="{{ url('how') }}">Как проходит консультация?</a></li>
+            </ul>
+
+          </div>
+          <div class="uk-card-footer">
+                <a href="{{route('yandex.create', ['type' => 5])}}" class="uk-button uk-button-text">Оплатить</a>
           </div>
       </div>
     </div>
